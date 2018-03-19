d53e8cfffd55caab0f09d5201fa84bf9b854b1b5
Maneeshachandran <maneesha.official@gmail.com>
2018-03-19 18:34:48 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+'use strict'
+const http = require('http')
+    , express = require('express')
+    , app = express()
+    , server = http.createServer(app);
+
+app.use(function(req, res, next) {
+ 	res.header("Access-Control-Allow-Origin", "*");
+ 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 	next();
+ 	});
+
+app.use(express.static('./../'));
+
+app.use('/',(req,res,next)=>{
+  console.log('inside routes');
+  next();
+},finalResult);
+
+module.exports = server.listen(3000, err => {
+  if(err){
+    throw err
+  }
+  console.log('Server running on 3000')
+})
